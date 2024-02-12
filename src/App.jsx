e4d24dd1491c4951445ceefa1d1db489b72b70a9
Mayank <mayank0real0world@gmail.com>
2024-02-12 12:17:32 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='bg-black h-[900px] w-full '>
+
+    <h1 className='text-white'>hobbycue</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
